5f7b1ec084452a7d848481ffa5c824e7f1ee2772
Harsh Shroff <151harshshroff@gmail.com>
2021-05-25 13:48:38 +0530

    initial commit

---
+++ textutils/views.py
@@ -0,0 +1,62 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse("Home")
+
+
+def analyze(request):
+    # getting the text
+    djtext = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    allcaps = request.GET.get('allcaps', 'off')
+    nlrem = request.GET.get('nlrem', 'off')
+    xsprem = request.GET.get('xsprem', 'off')
+
+    if removepunc == 'off' and allcaps == 'off' and nlrem == 'off' and xsprem == 'off':
+        return render(request, 'index.html')
+
+    if removepunc == 'on':
+        punctuations = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    if allcaps == 'on':
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Changed touppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    if nlrem == 'on':
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+        params = {'purpose': 'Changed to uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    if xsprem == 'on':
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index + 1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Extra space', 'analyzed_text': analyzed}
+        # return render(request, 'analyze.html', params)
+
+    # else:
+    #     return HttpResponse('ERROR')
+
+    return render(request, 'analyze.html', params)
+
+# def about(request):
+#     return render(request, 'about.html')
